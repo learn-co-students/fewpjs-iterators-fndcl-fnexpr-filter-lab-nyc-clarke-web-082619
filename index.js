63f9004f062d169d327db2f497e89e44428cacbe
jw20191n <clairejw1015@gmail.com>
2019-10-04 23:37:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+
+    let filtered_drivers = drivers.filter( n => { return n.toLowerCase() === string.toLowerCase() });
+    return filtered_drivers;
+}
+
+
+function fuzzyMatch(drivers, string){
+    let filtered_drivers = drivers.filter( n => {
+        return n.charAt(0).toLowerCase() === string.charAt(0).toLowerCase()
+    });
+    return filtered_drivers;
+}
+
+function matchName(drivers, string){
+    let filtered_drivers = drivers.filter( n => {
+        return n["name"] === string
+    });
+    return filtered_drivers;
+}
