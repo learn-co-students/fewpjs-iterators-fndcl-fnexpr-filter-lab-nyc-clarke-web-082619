8da5111c5a3caa59811c23f0d80d32b230ab573b
Evan Krone <evankrone@Evans-MBP-2.fios-router.home>
2019-10-06 18:27:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, string) {
+      return drivers.filter(n => n.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(char => char.startsWith(string));
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(n => n.name === string);
+
+}
