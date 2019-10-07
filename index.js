78f16176376e164c91f120bb4043457b0f452c98
github username <github email address>
2019-10-07 01:09:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+
+// let even = arr.filter(n => {
+//     return n % 2 === 0;
+//   });
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()});
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.startsWith(string)
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.name === string 
+    });
+}
+
+
+// function findMatching(drivers, name) {
+//     return drivers.filter((driver) => {return driver.toLowerCase() === name.toLowerCase()});
+// }
